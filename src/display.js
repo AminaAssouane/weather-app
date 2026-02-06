bbef77e8c770bc148218cf070216c9262d9ea0ef
Amina <minassouane@gmail.com>
2026-02-06 07:23:44 +0100

    Hide temperature when spinner is shown

--- src/display.js
+++ src/display.js
@@ -3,6 +3,7 @@ import * as logic from "./logic.js";
 const currentWeather = document.getElementById("current-weather");
 const weekWeather = document.getElementById("week-weather");
 let currentTemp = document.getElementById("current-temp");
+const loading = document.getElementById("loading");
 
 let cityInput = document.getElementById("city");
 
@@ -12,8 +13,17 @@ function displayCurrentWeather(weather) {
 }
 
 async function main(city) {
-  const weather = await logic.processWeather(city);
-  displayCurrentWeather(weather);
+  loading.style.display = "block";
+  currentTemp.style.display = "none";
+  try {
+    const weather = await logic.processWeather(city);
+    displayCurrentWeather(weather);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    currentTemp.style.display = "block";
+    loading.style.display = "none";
+  }
 }
 
 cityInput.addEventListener("keydown", (e) => {
